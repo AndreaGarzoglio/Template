688e978b819f5c89687da119926f5a7775a7c276
AndreaGarzoglio <77269125+AndreaGarzoglio@users.noreply.github.com>
2026-06-11 14:22:10 +0200

    added testing

---
+++ src/index.test.js
@@ -0,0 +1,12 @@
+/**
+ * Test suite for index.js
+ * Add your tests here to verify the functionality of your main code
+ */
+
+describe('Index', () => {
+    test('should pass', () => {
+        expect(true).toBe(true);
+    });
+
+    // Add your tests below
+});
